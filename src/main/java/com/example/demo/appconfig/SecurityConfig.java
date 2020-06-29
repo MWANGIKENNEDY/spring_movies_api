fe8e99134468e5a86f3f853e4f0b0417da75c0ec
MWANGIKENNEDY <kenmwan75@gmail.com>
2020-06-29 12:49:39 +0300

    Implemented H2/Spring-Security configuration

--- src/main/java/com/example/demo/appconfig/SecurityConfig.java
+++ src/main/java/com/example/demo/appconfig/SecurityConfig.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Autowired
     UserDetailsService userDetailsService;
@@ -24,15 +26,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-        .antMatchers("/api/movies").hasRole("ADMIN")
-        .antMatchers("/").permitAll()
+        .antMatchers("/api/**","/h2/**").permitAll()
         .and().formLogin();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
     }
-
-    
+        
 }
